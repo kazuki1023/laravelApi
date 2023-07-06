e46836c938eb97146d3f62dc531864cbe76266cc
kazuki1023 <k.keio12569@gmail.com>
2023-07-06 15:03:26 +0900

    add 取得できた

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use RakutenRws_Client;
 
 class PostController extends Controller
 {
     public function show(Request $request) {
-        dd($request->search);
+        // dd($request->search);
+        $client = new RakutenRws_Client();
+        define("RAKUTEN_APPLICATION_ID", config('app.rakuten_id'));
+        $client->setApplicationId($_ENV['RAKUTEN_APPLICATION_ID']);
+        // dd($client);
+
+        $keyword = $request->input('search');
+
+        $response = $client->execute('IchibaItemSearch', [
+            'keyword' => $keyword
+        ]);
+
+        if ($response->isOk()) {
+            echo $response['count']."件見つかりました。\n";
+
+            foreach ($response as $item) {
+                echo $item['itemName']."\n";
+            }
+        } else {
+            echo 'Error:'.$response->getMessage();
+        }
     }
 };
