117876d9f3835f22d01cc20eecc00ef364b3ca0f
kazuki1023 <k.keio12569@gmail.com>
2023-07-12 00:36:02 +0900

    add post取得

--- src/resources/views/result.blade.php
+++ src/resources/views/result.blade.php
@@ -58,7 +58,7 @@
                         <td class="px-6 py-4">
                             <button type="button"
                                 class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-                                onclick="location.href='{{ route('detail', ['id' => $item['itemCode']]) }}'">詳細</button>
+                                onclick="location.href='{{ route('detail' )}}?id={{ urlencode($item['itemCode']) }}'">詳細</button>
                         </td>
                     </tr>
                 @endforeach
